3471ba33d5efaa302edcc2f75a22119b3c479017
Ghadeer Al Jufout <ghadeer1112@yahoo.com>
2024-10-02 21:43:10 -0700

    intro.java
    Print statement yippie!

---
+++ intro.java
@@ -0,0 +1,14 @@
+/* To comment  Multiple lines
+*/
+
+// To comment a single line
+
+package practiceJava;
+
+ public class intro {
+	 
+	 public static void main(String[] args)
+	 {
+		 System.out.println("Welcome to Java!");
+	 }
+ }

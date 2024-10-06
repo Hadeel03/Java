a2bc2c7b5759f52ad4af5438548fdc5d2c9ab6c5
Ghadeer Al Jufout <ghadeer1112@yahoo.com>
2024-10-06 12:15:17 -0700

    First demo & another quiz added

---
+++ src/demos/week1/StringDemo.java
@@ -0,0 +1,24 @@
+package demos.week1; 
+
+import java.awt.Rectangle; 
+
+public class StringDemo 
+{
+	public static void main(String[] args)
+	{
+		String palindrome = "Dot saw I was Tod";
+		int len = palindrome.length();
+		System.out.println("String length is: " + len);
+		
+		System.out.println(palindrome.replace(' ', 'X'));
+		
+		System.out.println(palindrome.toLowerCase());
+		System.out.println(palindrome.toUpperCase());
+		
+		System.out.println(palindrome.substring(2));
+		System.out.println(palindrome.substring(4, 7));
+		
+		
+		
+	}
+}
